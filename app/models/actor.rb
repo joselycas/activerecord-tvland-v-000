e0aecf202f48281c0acaf670365c28b41a910787
Jos Castro <joselycas@gmail.com>
2018-09-11 15:37:51 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,9 +7,8 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    self.characters.collect do |s|
+    self.characters.shows.collect do |s|
       s.name
-      binding.pry
     end
   end
 end
